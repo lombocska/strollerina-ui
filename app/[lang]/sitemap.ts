32b681bd80fd60088779d48d6c336bf770eef4a4
monika.lombos <monika.lombos@commitsmart.com>
2024-06-27 13:56:55 +0200

    sitemap extended with dynamic routes

--- app/[lang]/sitemap.ts
+++ app/[lang]/sitemap.ts
@@ -1,20 +1,58 @@
 import siteMetadata from '@/data/siteMetadata'
 import { allBlogs } from 'contentlayer/generated'
+import { getAllCarSeats, getAllStrollers } from 'lib/data';
 import { MetadataRoute } from 'next'
 
-export default function sitemap(): MetadataRoute.Sitemap {
-    const siteUrl = siteMetadata.siteUrl
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+    const siteUrl = siteMetadata.siteUrl;
+    
     const blogRoutes = allBlogs
         .filter((post) => !post.draft)
         .map((post) => ({
             url: `${siteUrl}/${post.path}`,
             lastModified: post.lastmod || post.date,
-        }))
+            alternates: {
+                languages: {
+                    hu: `${siteUrl}/hu/${post.path}`, // Példában: `es` nyelvű alternatív link
+                    en: `${siteUrl}/en/${post.path}`, // Példában: `de` nyelvű alternatív link
+                },
+            },
+        }));
 
-    const routes = ['', 'blog', 'projects'].map((route) => ({
+    const routes = ['', 'blog', 'strollers', 'carseats', 'about', 'contact', "gdpr", "tags", "manuals"].map((route) => ({
         url: `${siteUrl}/${route}`,
         lastModified: new Date().toISOString().split('T')[0],
-    }))
+        alternates: {
+            languages: {
+                hu: `${siteUrl}/hu/${route}`, // Példában: `es` nyelvű alternatív link
+                en: `${siteUrl}/en/${route}`, // Példában: `de` nyelvű alternatív link
+            },
+        },
+    }));
 
-    return [...routes, ...blogRoutes]
+    const strollers = await getAllStrollers();
+    const strollerInfoRoutes = strollers.map((stroller) => ({
+        url: `${siteUrl}/strollers/${stroller.brandValue.toLowerCase()}/${encodeURIComponent(stroller.generatedId)}`,
+        lastModified: new Date().toISOString().split('T')[0],
+        alternates: {
+            languages: {
+                hu: `${siteUrl}/hu/strollers/${stroller.brandValue.toLowerCase()}/${encodeURIComponent(stroller.generatedId)}`,
+                en: `${siteUrl}/en/strollers/${stroller.brandValue.toLowerCase()}/${encodeURIComponent(stroller.generatedId)}`,
+            },
+        },
+    }));
+
+    const carseats = await getAllCarSeats();
+    const carseatInfoRoutes = carseats.map((carseat) => ({
+        url: `${siteUrl}/carseats/${carseat.brandValue.toLowerCase()}/${encodeURIComponent(carseat.generatedId)}`,
+        lastModified: new Date().toISOString().split('T')[0],
+        alternates: {
+            languages: {
+                hu: `${siteUrl}/hu/carseats/${carseat.brandValue.toLowerCase()}/${encodeURIComponent(carseat.generatedId)}`,
+                en: `${siteUrl}/en/carseats/${carseat.brandValue.toLowerCase()}/${encodeURIComponent(carseat.generatedId)}`,
+            },
+        },
+    }));
+
+    return [...routes, ...blogRoutes, ...strollerInfoRoutes, ...carseatInfoRoutes];
 }
--- app/[lang]/strollers/page.tsx
+++ app/[lang]/strollers/page.tsx
@@ -3,17 +3,25 @@ import { getAllStrollers, getBrands } from 'lib/data';
 import StrollersContent from '@/components/strollerina/content/strollers_content';
 import { getDictionary } from 'get-dictionary';
 import { Locale } from 'next/dist/compiled/@vercel/og/satori';
+import { genPageMetadata } from '../seo';
+
+export const metadata = genPageMetadata({
+  title: 'Strollers',
+  description: 'Dynamic stroller comparison',
+  robots: {
+      index: true,
+      follow: true,
+  },
+})
 
 export default async function Strollers({
   params: { lang },
 }: {
   params: { lang: Locale };
 }) {
-
   const dictionary = await getDictionary(lang);
   const initialData = await getAllStrollers();
   const brands = await getBrands();
-
   return (
       <>
         <StrollersContent initialData={initialData} brands={brands} dictionary={dictionary.strollers}/>
