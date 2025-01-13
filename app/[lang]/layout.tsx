aee005d3f3173c0ccc6a0a2e562880e9ed1bb636
monika.lombos <lombos.monika@gmail.com>
2025-01-13 14:36:41 +0100

    GTM applied head and body

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -21,7 +21,7 @@ import { getDictionary } from 'get-dictionary'
 // import CookieConsentComponent from '../components/CookieConsent';
 
 export async function generateStaticParams() {
-  return i18n.locales.map((locale) => ({ lang: locale }));
+    return i18n.locales.map((locale) => ({ lang: locale }));
 }
 
 export const metadata: Metadata = {
@@ -75,67 +75,52 @@ export const metadata: Metadata = {
     },
 }
 
-export default async function RootLayout({ children, params, }: { children: React.ReactNode; params: { lang: Locale }; }) {
+export default async function RootLayout({ children, params }: { children: React.ReactNode; params: { lang: Locale } }) {
     const dictionary = await getDictionary(params.lang);
+  
     return (
-        <html
-            lang={params.lang}
-            className={`${font.variable} scroll-smooth`}
-            suppressHydrationWarning
-        >
-            <link
-                rel="apple-touch-icon"
-                sizes="76x76"
-                href="/static/favicons/apple-touch-icon.png"
-            />
-            <link
-                rel="icon"
-                type="image/png"
-                sizes="32x32"
-                href="/static/favicons/favicon-32x32.png"
-            />
-            <link
-                rel="icon"
-                type="image/png"
-                sizes="16x16"
-                href="/static/favicons/favicon-16x16.png"
-            />
-
-            <link rel="manifest" href="/static/favicons/site.webmanifest" />
-            <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
-            <meta name="msapplication-TileColor" content="#000000" />
-            <meta name="theme-color" media="(prefers-color-scheme: light)" content="#E9D3B6" />
-            <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000000" />
-            <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-            {/* <Script type="text/javascript"  src="//cdn.cookie-script.com/s/9daf868f81da50915192beccc1e1edc3.js"/> */}
-            <Script 
-                async 
-                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1946644893911245" 
-                crossOrigin='anonymous'
-                strategy="lazyOnload"
-            />
-
-            <body className="bg-background antialiased ">
-                <ThemeProviders>
-                    <Analytics />
-                        <div className="flex flex-col min-h-screen">
-                            <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
-                            {/* <Header /> */}
-                            <Navbar dictionary={dictionary}/>
-                            <main className="flex-grow container mx-auto max-w-7xl px-6 mt-[80px]">
-                                {children}
-                            </main>
-                            <Footer />
-                            </SearchProvider>
-                        </div>
-                </ThemeProviders>
-                
-                {/* <Script
-                    src="https://cdn.cookie-script.com/s/9daf868f81da50915192beccc1e1edc3.js"
-                    strategy="beforeInteractive"
-             /> */}
-            </body>
-            <GoogleAnalytics gaId="G-M1NBC50F94" />
-        </html>
-    )
-}
+      <html
+        lang={params.lang}
+        className={`${font.variable} scroll-smooth`}
+        suppressHydrationWarning
+      >
+        {/* GTM Header */}
+        <Script
+          id="google-tag-manager"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N8QDRF8F');`,
+          }}
+        />
+  
+        <body className="bg-background antialiased ">
+          {/* GTM Body */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-N8QDRF8F"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+          </noscript>
+  
+          <ThemeProviders>
+            <Analytics />
+            <div className="flex flex-col min-h-screen">
+              <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
+                <Navbar dictionary={dictionary} />
+                <main className="flex-grow container mx-auto max-w-7xl px-6 mt-[80px]">
+                  {children}
+                </main>
+                <Footer />
+              </SearchProvider>
+            </div>
+          </ThemeProviders>
+        </body>
+      </html>
+    );
+  }
+  
