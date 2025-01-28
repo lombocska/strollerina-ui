867ee530a83a718c39ee8f78fc41dd074db2afc5
monika.lombos <lombos.monika@gmail.com>
2025-01-28 11:48:10 +0100

    fixing pid

--- app/[lang]/GoogleAdsense.tsx
+++ app/[lang]/GoogleAdsense.tsx
@@ -11,7 +11,7 @@ const GoogleAdsense: React.FC<Props> = ({ pId }) => {
   return (
     <Script
       async
-      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${pId}`}
       crossOrigin="anonymous"
       strategy="afterInteractive"
     />
